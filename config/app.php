ae0416eb1f3c3056ded5bbc38375fbd9e6d37f15
Azrulnizam <azrulnizam@m3online.com>
2019-09-03 23:51:11 +0800

    config azrul

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => false,
+    'debug' => true,
 
     /**
      * Configure basic information about the application.
@@ -215,7 +215,7 @@ return [
             'tls' => null,
         ],
     ],*/
-	'EmailTransport' => [
+    'EmailTransport' => [
         'default' => [
             'className' => 'Smtp',
             // The following keys are used in SMTP transports
@@ -226,13 +226,13 @@ return [
             'password' => 'AX@3PuP?bsJ8',
             'client' => null,
             'tls' => true,
-			'context' => [
-				'ssl' => [
-					'verify_peer' => false,
-					'verify_peer_name' => false,
-					'allow_self_signed' => true
-				]
-			],
+            'context' => [
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                ]
+            ],
         ],
     ],
 
@@ -253,7 +253,7 @@ return [
             'headerCharset' => 'utf-8',
         ],
     ],
-	/* 'Email' => [
+    /* 'Email' => [
         'default' => [
             'transport' => 'default',
             'from' => ['azrulbun@gmail.com'=>'Azrul Support'],
@@ -272,15 +272,15 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => '127.0.0.1',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
-            //'port' => 'nonstandard_port_number',
+            'port' => '8889',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'database' => 'e_diary',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
@@ -322,44 +322,6 @@ return [
             'timezone' => 'UTC',
             'cacheMetadata' => true,
             'quoteIdentifiers' => false,
-            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
-        ],
-		
-		'db_connect' => [
-            'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
-            'persistent' => false,
-            'host' => '10.0.46.12',
-            /**
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'nonstandard_port_number',
-            'username' => 'sa',
-            'password' => 'Akn362!FnKsZ',
-            'database' => 'fmt_v3',
-            'encoding' => 'utf8',
-            'timezone' => 'UTC',
-            'cacheMetadata' => true,
-
-            /**
-             * Set identifier quoting to true if you are using reserved words or
-             * special characters in your table or column names. Enabling this
-             * setting will result in queries built using the Query Builder having
-             * identifiers quoted when creating SQL. It should be noted that this
-             * decreases performance because each query needs to be traversed and
-             * manipulated before being executed.
-             */
-            'quoteIdentifiers' => false,
-
-            /**
-             * During development, if using MySQL < 5.6, uncommenting the
-             * following line could boost the speed at which schema metadata is
-             * fetched from the database. It can also be set directly with the
-             * mysql configuration directive 'innodb_stats_on_metadata = 0'
-             * which is the recommended value in production environments
-             */
             //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
         ]
     ],

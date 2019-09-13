74f4e3184692b410cd69717b7ec5aaa3c3ee6452
azrulniza <azrulnizam.ghani@gmail.com>
2019-09-13 18:08:10 +0800

    List attendances

--- src/Controller/AttendancesController.php
+++ src/Controller/AttendancesController.php
@@ -6,6 +6,8 @@ use App\Controller\AppController;
 use Cake\I18n\Time;
 use Cake\ORM\Query;
 use Cake\ORM\Table;
+use Cake\Datasource\ConnectionManager;
+
 /**
  * Attendances Controller
  *
@@ -22,42 +24,23 @@ class AttendancesController extends AppController
      */
     public function index()
     {
+        // my_connection is defined in your database config
+        $conn = ConnectionManager::get('default');
+
         $this->loadModel('Users');
         $this->loadModel('Attendances');
         $this->loadModel('AttendanceLogs');
         $this->loadModel('Organizations');
+        $this->loadModel('UserOrganizations');
         $this->loadModel('Designations');
 
-        $this->paginate = [
-            'contain' => ['Users', 'AttendanceCodes']
-        ];
-
-    $query = $this->Attendances->find()
-            ->join([
-                'u' => [
-                    'table' => 'users',
-                    'type' => 'LEFT',
-                    'conditions' => 'u.id = attendances.user_id',
-                ],
-                'uo' => [
-                    'table' => 'users_organization',
-                    'type' => 'LEFT',
-                    'conditions' => 'uo.user_id = u.id',
-                ],
-                'o' => [
-                    'table' => 'Organizations',
-                    'type' => 'LEFT',
-                    'conditions' => 'o.id = uo.organization_id',
-                ]
-            ]);
-
-        //$attendances = $this->paginate($this->Attendances);
-            $attendances = $this->paginate($query);
-
+    
         $userPIC = $this->AuthUser->id();
         $user = $this->Users->find()->contain(['Roles'])->Where(['id' => "$userPIC"])->limit(1)->first();
         $userRoles = $this->Users->Roles->initRolesChecker($user->roles);
-        
+        $usersOrganization = $this->UserOrganizations->find()->Where(['UserOrganizations.user_id' => "$userPIC"])->limit(1)->first();
+        $user_organization_id=$usersOrganization->organization_id;
+
         if ($userRoles->hasRole(['Master Admin'])) {
             $users = $this->Users->find('list');
         }else  if ($userRoles->hasRole(['Supervisor'])) {
@@ -69,12 +52,42 @@ class AttendancesController extends AppController
         }else  if ($userRoles->hasRole(['Admin'])) {
             $users = $this->Users->find('list')->where(['report_to'=>$userPIC]);
         }elseif ($userRoles->hasRole(['Staff'])) {
-            $has_attend = $this->Attendances->find('all',array('conditions'=>array('Attendances.user_id'=>"$userPIC", "DATE(Attendances.cdate)=CURDATE()")))->order(['Attendances.cdate'=>'DESC'])->contain(['Users'])->limit(1)->first();
+            /*$sql="SELECT Attendances.*, attendance_codes.`name` AS attendance_codes_name, Users.name AS username, Users.`report_to`, designations.`id` AS desgination_id, designations.`name` AS designation_name, designations.`gred` AS designation_gred, organizations.`name` AS organization_name, organizations.`id` AS organization_id
+            FROM Attendances
+            JOIN attendance_codes ON attendance_codes.`id`=attendances.`attendance_code_id`
+            JOIN Users ON Attendances.`user_id`= Users.id
+            JOIN User_designations ON user_designations.`user_id`= Users.id
+            JOIN designations ON designations.id = user_designations.`designation_id`
+            JOIN organizations ON organizations.id = designations.`organization_id`
+            WHERE attendances.`user_id`=$userPIC AND DATE(attendances.cdate)=CURDATE()";*/
+            $sql="SELECT Attendances.*, attendance_codes.`name` AS attendance_codes_name, Users.name AS username, Users.`report_to`, designations.`id` AS desgination_id, designations.`name` AS designation_name, designations.`gred` AS designation_gred, organizations.`name` AS organization_name, organizations.`id` AS organization_id FROM
+                Users 
+                JOIN user_organizations ON user_organizations.`user_id`=users.id
+                JOIN `user_designations`ON `user_designations`.`user_id`=users.id
+                JOIN `designations` ON `designations`.id=`user_designations`.designation_id
+                JOIN organizations ON organizations.id = designations.`organization_id`
+                RIGHT JOIN `attendances` ON attendances.`user_id`= users.id
+                JOIN attendance_codes ON attendance_codes.`id`=attendances.`attendance_code_id`
+                WHERE DATE(attendances.cdate)=CURDATE()";
+            $stmt = $conn->execute($sql);
+            $attendances_in = $stmt->fetchAll('assoc');
 
-            $user = $this->Users->find('all')->Where(['id'=>"$userPIC"])->limit(1)->first();
+            $attendances=array();
+            
+            foreach($attendances_in as $attendance_in){
+                $attendances_std=new \stdClass();
+
+                $has_in = $this->Attendances->find('all')->Where(['user_id'=>$attendance_in['user_id']])->where(['status'=>1])->where(["DATE(cdate)=CURDATE()"])->limit(1)->first();
+                $has_out = $this->Attendances->find('all')->Where(['user_id'=>$attendance_in['user_id']])->where(['status'=>2])->where(["DATE(cdate)=CURDATE()"])->limit(1)->first();
+
+                $attendance_in['in']=$has_in->cdate;
+                $attendance_in['out']=$has_out->cdate;
+
+                $attendances[$attendance_in['user_id']]=$attendance_in;
+            }
         }
-
-        $this->set(compact('attendances'));
+        
+        $this->set(compact('attendances','userRoles','attendances_in'));
     }
 
     /**
@@ -138,8 +151,8 @@ class AttendancesController extends AppController
             $data = $this->request->data;
 
             $attendance = $this->Attendances->patchEntity($attendance, $this->request->getData());
-            $attendance->cdate = $now->i18nFormat('yyyy-MM-dd HH:mm:ss');
-            $attendance->mdate = $now->i18nFormat('yyyy-MM-dd HH:mm:ss');
+            //$attendance->cdate = $now->i18nFormat('yyyy-MM-dd HH:mm:ss');
+            //$attendance->mdate = $now->i18nFormat('yyyy-MM-dd HH:mm:ss');
             $attendance->pic = $userPIC;
             $action=$data['action'];
             $user_id=$data['user_id'];
@@ -154,6 +167,7 @@ class AttendancesController extends AppController
 
             if ($this->Attendances->save($attendance)) {
                 $id=$this->Attendances->save($attendance)->id;
+                $cdate=$this->Attendances->save($attendance)->cdate;
                 if($action=='in'){
                     $attendanceLogTable = TableRegistry::get('AttendanceLogs');
                     $attendance_log = $attendanceLogTable->newEntity();
@@ -161,8 +175,8 @@ class AttendancesController extends AppController
                     $attendance_log->user_id=$user_id;
                     $attendance_log->attendance_code_id=1;
                     $attendance_log->pic=$userPIC;
-                    $attendance_log->cdate=$now->i18nFormat('yyyy-MM-dd HH:mm:ss');
-                    $attendance_log->mdate=$now->i18nFormat('yyyy-MM-dd HH:mm:ss');
+                    $attendance_log->cdate=$cdate;
+                    $attendance_log->mdate=$cdate;
 
                     $this->AttendanceLogs->save($attendance_log);
 
@@ -176,8 +190,8 @@ class AttendancesController extends AppController
                     $attendance_log->attendance_code_id=2;
                     $attendance_log->pic=$userPIC;
                     $attendance_log->status=2;
-                    $attendance_log->cdate=$now->i18nFormat('yyyy-MM-dd HH:mm:ss');
-                    $attendance_log->mdate=$now->i18nFormat('yyyy-MM-dd HH:mm:ss');
+                    $attendance_log->cdate=$cdate;
+                    $attendance_log->mdate=$cdate;
 
                     $this->AttendanceLogs->save($attendance_log);
 
--- src/Template/Attendances/index.ctp
+++ src/Template/Attendances/index.ctp
@@ -8,53 +8,65 @@
                         <thead>
                             <tr>
                                 <th><?= __('No') ?></th>
-                                <th><?= $this->Paginator->sort('user_id', 'Name') ?></th>
-                                <th><?= $this->Paginator->sort('attendance_code_id','Status') ?></th>
+                                <th><?= __('Name') ?></th>
                                 <th><?= __('Department') ?></th>
+                                <th><?= __('Status') ?></th>
                                 <th><?= __('In') ?></th>
                                 <th><?= __('Out') ?></th>
-                                <th><?= $this->Paginator->sort('gps_lng') ?></th>
-                                <th><?= $this->Paginator->sort('pic') ?></th>
-                                <th class="actions"><?= __('Actions') ?></th>
+                                <?php if($userRoles->hasRole(['Master Admin','Admin','Supervisor'])) :?>
+                                    <th class="actions"><?= __('Actions') ?></th>
+                                <?php endif; ?>
                             </tr>
                         </thead>
                         <tbody class="ui-sortable">
+                        <?php //print_r($attendances);?>
                         <?php $count = 0 ?>
-                        <?php foreach ($attendances as $key => $attendance): ?>
+        
+                        <?php foreach ($attendances as $attendance): ?>
                             
-                            <tr id="<?= $attendance->id; ?>" class="<?= (++$count%2 ? 'odd' : 'even') ?>">
+                            <tr id="<?= $attendance['id']; ?>" class="<?= (++$count%2 ? 'odd' : 'even') ?>">
                                 <td><?= $this->Number->format($count) ?></td>
                                 <td>
-                                    <?= $attendance->has('user') ? $this->Html->link($attendance->user->name, ['controller' => 'Users', 'action' => 'view', $attendance->user->id]) : '' ?>
-                                </td>
+                                    <?= $attendance['username']?>
+                                </td>   
+                                <td><?= $attendance['organization_name'] ?></td>
                                 <td>
-                                    <?= $attendance->has('attendance_code') ? $this->Html->link($attendance->attendance_code->name, ['controller' => 'AttendanceCodes', 'action' => 'view', $attendance->attendance_code->id]) : '' ?>
+                                    <?php 
+                                    if($attendance['status']==2){ 
+                                        $color='red';
+                                    }else if($attendance['status']==1){
+                                        $color='green';
+                                    }
+                                    ?>
+                                    <p style="color:<?php echo $color;?>"><?= $attendance['attendance_codes_name'] ?></p>
                                 </td>
-                                <td><?= h($attendance->ip_address) ?></td>
-                                <td><?= $this->Number->format($attendance->gps_lat) ?></td>
-                                <td><?= $this->Number->format($attendance->gps_lng) ?></td>
-                                <td><?= $this->Number->format($attendance->pic) ?></td>
-                                <td class="actions">
-                                    <div class="btn-group">
-                                        <?= $this->Html->link($this->Html->tag('i', '', ['class' => 'fa fa-eye']), ['action' => 'view', $attendance->id], ['escape' => false, 'title' => __('View'), 'class' => 'btn btn-info btn-xs']) ?>
-                                        <?= $this->Html->link($this->Html->tag('i', '', ['class' => 'fa fa-pencil']), ['action' => 'edit', $attendance->id], ['escape' => false, 'title' => __('Edit'), 'class' => 'btn btn-success btn-xs']) ?>
-                                        <?= $this->Form->postLink($this->Html->tag('i', '', ['class' => 'fa fa-trash']), ['action' => 'delete', $attendance->id], ['escape' => false, 'title' => __('Delete'), 'class' => 'btn btn-danger btn-xs', 'confirm' => __('Are you sure you want to delete this {0}?', 'attendance')]) ?>
-                                    </div>
-                                </td>
+                                <td><?php if($attendance['in']!=""){
+                                     $date_in=date_create($attendance['in']);
+                                echo date_format($date_in,"H:i a");
+                                     
+                                }
+                               
+                                ?></td>
+                                <td><?php if($attendance['out']!=""){
+                                    $date_out=date_create($attendance['out']);
+                                        echo date_format($date_out,"H:i a");
+                                }
+                                
+                                ?></td>
+                               
+                               <?php if($userRoles->hasRole(['Master Admin','Admin','Supervisor'])) :?>
+                                    <td class="actions">
+                                        <div class="btn-group">
+                                            <?= $this->Html->link($this->Html->tag('i', '', ['class' => 'fa fa-eye']), ['action' => 'view', $attendance->id], ['escape' => false, 'title' => __('View'), 'class' => 'btn btn-info btn-xs']) ?>
+                                            <?= $this->Html->link($this->Html->tag('i', '', ['class' => 'fa fa-pencil']), ['action' => 'edit', $attendance->id], ['escape' => false, 'title' => __('Edit'), 'class' => 'btn btn-success btn-xs']) ?>
+                                            <?= $this->Form->postLink($this->Html->tag('i', '', ['class' => 'fa fa-trash']), ['action' => 'delete', $attendance->id], ['escape' => false, 'title' => __('Delete'), 'class' => 'btn btn-danger btn-xs', 'confirm' => __('Are you sure you want to delete this {0}?', 'attendance')]) ?>
+                                        </div>
+                                    </td>
+                                <?php endif; ?>
                             </tr>
                         <?php endforeach ?>
                         </tbody>
                     </table>
-                </div>
-            </div>
-            <div class="box-footer">
-                <div class="paginator">
-                    <ul class="pagination">
-                        <?= $this->Paginator->prev('< ' . __('previous')) ?>
-                        <?= $this->Paginator->numbers() ?>
-                        <?= $this->Paginator->next(__('next') . ' >') ?>
-                    </ul>
-                    <p><?= $this->Paginator->counter() ?></p>
                 </div>
             </div>
         </div>
